c4d25ac801461f934f1cdeb415fb71d5ee38a9ef
Luisk <ldelvalleu@ucenfotec.ac.cr>
2018-04-21 01:42:35 -0600

    Creación de vistas

--- public/route.js
+++ public/route.js
@@ -9,7 +9,7 @@
 
     $stateProvider
      .state('landingPage', {
-        url: '/landingPage',
+        url: '/',
         templateUrl: './components/landingPage/landingPage.view.html',
         data:{
           pageTitle: 'Hoteleria'
@@ -21,6 +21,21 @@
         templateUrl: './components/users/registerUsers/registerUsers.view.html',
         data:{
           pageTitle: 'Registrar usuario'
+        }/*,
+        resolve: {
+          load: ['$ocLazyLoad', ($ocLazyLoad) => {
+            return $ocLazyLoad.load('./components/main/main.controller.js')
+          }]
+        },
+        controller: 'controladorMain',
+        controllerAs: 'vm'*/
+      })
+
+      .state('logIn', {
+        url: '/logIn',
+        templateUrl: './components/logIn/logIn.view.html',
+        data:{
+          pageTitle: 'Inicio de sesión'
         }/*,
         resolve: {
           load: ['$ocLazyLoad', ($ocLazyLoad) => {
